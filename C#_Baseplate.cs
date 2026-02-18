9b5a213f155ffb95553a61af79156778ca261dd4
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-02-18 14:23:43 +0200

    Create C#_Baseplate.cs

---
+++ C#_Baseplate.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+//                                --- This is my C# starting baseplate ---
+//            --- It is made for me cuz im lazy and copy pasting code snippets is easier ---
+//namespace Name_of_program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            int rowI = 0;//amuda ^ 0
+            int collumn_ = 0;//shura > 1
+            // || amuda
+            // -- shura
+            int[] arr = new int[length];
+            int[,] matrix = new int[rowI,collumn_];
+            /*
+            (row, column)
+            (0,0) (0,1) (0,2)
+            (1,0) (1,1) (1,2)
+            (2,0) (2,1) (2,2)
+            */
+            // matrix.GetLength(1) -> collumn
+            // matrix.GetLength(0) -> row
+            int num = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine($"");
+          
+        }
+    }
