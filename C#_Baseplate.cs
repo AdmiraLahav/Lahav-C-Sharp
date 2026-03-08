b200cee1277aa99fac202c470c3e2d550f7f1c8c
LH <113438937+AdmiraLahav@users.noreply.github.com>
2026-03-08 18:27:18 +0200

    Update C#_Baseplate.cs

--- C#_Baseplate.cs
+++ C#_Baseplate.cs
@@ -10,24 +10,36 @@ class Program
 {
     static void Copy_Paste()
     {
+        //colors cuz cool - > https://raw.githubusercontent.com/fidian/ansi/master/images/color-codes.png
+        string RESET = "\x1b[0m";
+        string blue_color = "\x1b[38;5;12m";
+        string red_color = "\x1b[38;5;196m";
+        string yellow_color = "\x1b[38;5;226m";
+        string INFO = $"[{blue_color}INF{RESET}]";
+        string ERROR = $"[{red_color}ERR{RESET}]";
+        string WARNING = $"[{yellow_color}WRN{RESET}]";
+
+        string Func_Readline = Console.ReadLine();
+
         int length = 0;
         int rowI = 2;//amuda ^ 0
         int collumn_ = 3;//shura > 1
-                         // || amuda
-                         // -- shura
+
         int[] arr = new int[length];
         int[,] matrix = new int[rowI, collumn_];
         // matrix.GetLength(1) -> collumn
         // matrix.GetLength(0) -> row
         //int num = int.Parse(Console.ReadLine());
 
-        Console.WriteLine($"");
+        //input order
+        //1 2 3
+        //4 5 6
         for (int FrowI = 0; FrowI < matrix.GetLength(0); FrowI++)
         {
             for (int Fcollumn_ = 0; Fcollumn_ < matrix.GetLength(1); Fcollumn_++)
             {
                 Console.WriteLine($"ROW {FrowI}    COL {Fcollumn_}");
-                matrix[FrowI, Fcollumn_] = int.Parse(Console.ReadLine());
+                matrix[FrowI, Fcollumn_] = int.Parse(Func_Readline);
             }
         }
         //debug - write the whole matrix
@@ -42,5 +54,6 @@ class Program
     }
     static void Main(string[] args)
     {
+        
     }
 }
